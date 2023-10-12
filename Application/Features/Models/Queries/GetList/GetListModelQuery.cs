9c11e6bea2fbed2a268735245589f26d44dd340a
Ömer Çetin <94043864+omercetinn@users.noreply.github.com>
2023-10-12 16:07:32 +0300

    list

---
+++ Application/Features/Models/Queries/GetList/GetListModelQuery.cs
@@ -0,0 +1,24 @@
+﻿using Core.Application.Requests;
+using Core.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Models.Queries.GetList
+{
+    public class GetListModelQuery:IRequest<GetListResponse<GetListModelListItemDto>>
+    {
+        public PageRequest PageRequest { get; set; }
+        public class GetListModelQueryHandler:IRequestHandler<GetListModelQuery, GetListResponse<GetListModelListItemDto>> 
+        {
+           
+            public Task<GetListResponse<GetListModelListItemDto>> Handle(GetListModelQuery request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
