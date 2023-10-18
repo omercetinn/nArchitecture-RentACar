dfd6a631a21f7a83290d4d11e043fc3d03502a91
Ömer Çetin <94043864+omercetinn@users.noreply.github.com>
2023-10-18 10:42:06 +0300

    business rules

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Core.Application.Rules;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +15,28 @@ namespace Application
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), typeof(BaseBusinessRules));
             services.AddMediatR(configuration =>
             {
                 configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
             });
+            return services;
+        }
+        //Ayağa kalkınca direkt burayı çalıştır ki kurallara göre hareket etsin
+        public static IServiceCollection AddSubClassesOfType(
+     this IServiceCollection services,
+     Assembly assembly,
+     Type type,
+     Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null
+ )
+        {
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+            foreach (var item in types)
+                if (addWithLifeCycle == null)
+                    services.AddScoped(item);
+
+                else
+                    addWithLifeCycle(services, type);
             return services;
         }
     }
